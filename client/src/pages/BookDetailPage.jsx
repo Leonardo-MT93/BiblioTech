b11523cbe5fb5a699026fd9f52a9cdeb71f9e814
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2024-03-13 17:07:29 -0300

    Extrayendo libros de la base de datos y los detalles de cada uno

--- client/src/pages/BookDetailPage.jsx
+++ client/src/pages/BookDetailPage.jsx
@@ -1,26 +1,48 @@
 import { useParams } from "react-router-dom";
-import { books } from "../constants/books";
+// import { books } from "../constants/books";
+import { useEffect, useState } from "react";
 
 const BookDetailPage = () => {
   const { id } = useParams();
+  const [selectedBook, setSelectedBook] = useState(null);
 
-  const book = books.find((book) => book.id === parseInt(id));
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:9000/api/book/${id}`);
+        if (!response.ok) {
+          throw new Error("Error al obtener los libros");
+        }
+        const { searchedBook } = await response.json();
+        setSelectedBook(searchedBook);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
   return (
     <div className="w-full h-[60vh] flex flex-col items-center justify-around ">
       <h1 className="text-3xl font-bold  border-b-black">Detalles</h1>
       <div className="max-w-4xl h-[40vh] flex flex-col justify-around p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <h2 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {`Título: ${book.title}`}
-        </h2>
-        <h3 className="mb-3 text-lg font-normal text-gray-700 dark:text-gray-400">
-          {`Autor: ${book.author}`}
-        </h3>
-        <h3 className="mb-3 text-lg font-normal text-gray-700 dark:text-gray-400">
-          {`Género: ${book.genre}`}
-        </h3>
-        <h3 className="mb-3 text-lg font-normal text-gray-700 dark:text-gray-400">
-          {`Año: ${book.year}`}
-        </h3>
+        {selectedBook && (
+          <>
+            <h2 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {`Título: ${selectedBook.title}`}
+            </h2>
+            <h3 className="mb-3 text-lg font-normal text-gray-700 dark:text-gray-400">
+              {`Autor: ${selectedBook.author}`}
+            </h3>
+            <h3 className="mb-3 text-lg font-normal text-gray-700 dark:text-gray-400">
+              {`Género: ${selectedBook.genre}`}
+            </h3>
+            <h3 className="mb-3 text-lg font-normal text-gray-700 dark:text-gray-400">
+              {`Año: ${selectedBook.year}`}
+            </h3>
+          </>
+        )}
       </div>
     </div>
   );
--- client/src/pages/BooksListPage.jsx
+++ client/src/pages/BooksListPage.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import BookCard from "../components/BookCard";
-import { books } from "../constants/books";
+// import { books } from "../constants/books";
 
 const BooksListPage = () => {
+  const [books, setBooks] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:9000/api/book');
+        if (!response.ok) {
+          throw new Error("Error al obtener los libros");
+        }
+        const {books} = await response.json();
+        console.log(books)
+        setBooks(books);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {books.map((book) => (
-        <BookCard key={book.id} genre={book.genre} title={book.title} id={book.id} bookUserId={book.bookUserId}/>
+        <BookCard key={book._id} genre={book.genre} title={book.title} id={book._id} bookUserId={book.created_by}/>
       ))}
     </div>
   );
